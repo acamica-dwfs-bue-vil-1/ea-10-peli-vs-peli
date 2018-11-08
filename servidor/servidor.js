e0d7b00c66bad139f367225559b2e998198b9e28
EitanFuturo <llovia@gmail.com>
2018-11-08 17:33:23 -0300

    Commit inicial con entorno configurado.

---
+++ servidor/servidor.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+var app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+//seteamos el puerto en el cual va a escuchar los pedidos la aplicación
+const puerto = '8080';
+
+app.listen(puerto, function () {
+  console.log( "Escuchando en el puerto " + puerto );
+});
